782d544397aaf5c77203b9f5ff18ea47832986ec
Metsehafe Tesfu <thephenompro@gmail.com>
2023-03-31 14:09:28 +0300

    part 1 complete

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,6 +2,14 @@ const Header = ({course}) => {
     return <h1>{course}</h1>
 }
 
+const Content = () => {
+    
+}
+
+const Total = ({total}) => {
+
+}
+
 const App = () => {
     const course = 'Half Stack application development'
     const part1 = 'Fundamentals of React'
@@ -13,16 +21,8 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content />
+      <Total />
     </div>
   )
 }
