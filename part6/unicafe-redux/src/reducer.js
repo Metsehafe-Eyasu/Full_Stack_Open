7f7514849ee5c295223f316b688b4b53763e96f4
Metsehafe Tesfu <metsehafe.eyasu@aastustudent.edu.et>
2023-07-17 11:24:39 +0300

    Part 6: Exercises 6.1 - 6.2 complete

---
+++ part6/unicafe-redux/src/reducer.js
@@ -0,0 +1,25 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+      const {good} = state
+      return {...state, good: good + 1}
+    case 'OK':
+      const {ok} = state
+      return {...state, ok: ok + 1}
+    case 'BAD':
+      return {...state, bad: state.bad + 1}
+    case 'ZERO':
+      return initialState
+    default: return state
+  }
+  
+}
+
+export default counterReducer
